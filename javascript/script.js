3e748543937f9045d398d674d5d1d793fa3e02da
handaxhanda <Mokurenntatami@outlook.jp>
2024-12-02 20:51:14 +0900

    update site

--- javascript/script.js
+++ javascript/script.js
@@ -17,13 +17,5 @@ if (hour >= 6 && hour < 9) {
     timemessage = ('こんばんは');
 } else if (hour >= 0 && hour < 6) {
     timemessage = ('こんばんは');
-    const pasword = ('こんばんは');
-    let pasmessage;
-    const rep = window.prompt('こんばんは');
-    if (pasword === rep) {
-        pasmessage = ('なかなかやりますわねっ');
-        document.getElementById("openpage").innerHTML = ("<a href=html/secretdiary.html>PressToEnterTheSecretPage</a>");
-    }
-    window.alert(pasmessage);
 }
 document.getElementById("greeting").innerHTML = timemessage;
